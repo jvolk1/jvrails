726000eb362b0611b2b0ec22c9d09f816673a873
jay volk <jayvolk@gmail.com>
2013-03-22 16:23:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jvrails::Application.config.secret_token = 'a245bb69751391dbac55c727e51ffe40072f9db408e83a7779f46d032a10559ffa27ed0cd0b39d3a49ef2a02c5d2857cb9c4b71b5fcd814c0f4596700a9cb0a3'
